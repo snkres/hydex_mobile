a09805569694ecf4a0f011836c4f2aa4024d4311
MezoPeeta <wildlifemain1@gmail.com>
2025-07-15 13:50:28 +0300

    adding widget book

---
+++ lib/widgetbook/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:hydex/widgetbook/app.directories.g.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+@widgetbook.App()
+class WidgetbookApp extends StatelessWidget {
+  const WidgetbookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Widgetbook.material(
+      directories: directories,
+    );
+  }
+}
