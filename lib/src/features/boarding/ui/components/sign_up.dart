dcdd7dc47c8b5809d87848ca3fd2c26479d5b395
MezoPeeta <wildlifemain1@gmail.com>
2025-08-21 15:02:58 +0300

    add done button in ios

--- lib/src/features/boarding/ui/components/sign_up.dart
+++ lib/src/features/boarding/ui/components/sign_up.dart
@@ -30,168 +30,179 @@ class _SignUpComponentState extends State<SignUpComponent> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+      physics: NeverScrollableScrollPhysics(),
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          minHeight: MediaQuery.sizeOf(context).height - 100,
+        ),
+        child: IntrinsicHeight(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
-                "Sign up",
-                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 22),
-              ),
-              SizedBox(height: 8),
-              Text(
-                "Access the city’s most coveted spots, luxury deals, and curated experiences.",
-                style: TextStyle(fontSize: 14, color: Color(0xff7A7F99)),
-              ),
-              SizedBox(height: 16),
-              Consumer(
-                builder: (context, ref, child) {
-                  final selectedCountry = ref.watch(
-                    countryPickerNotifierProvider,
-                  );
-                  return selectedCountry.when(
-                    data: (data) {
-                      return Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        spacing: 8,
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              showModalBottomSheet(
-                                context: context,
-                                isScrollControlled: true,
-                                builder: (context) =>
-                                    CountryPickerBottomSheet(),
-                              );
-                            },
-                            child: Container(
-                              height: 80,
-                              padding: EdgeInsets.all(15),
-                              decoration: BoxDecoration(
-                                color: Theme.of(
-                                  context,
-                                ).colorScheme.secondaryContainer,
-                                borderRadius: BorderRadius.circular(16),
-                              ),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                spacing: 10,
-                                children: [
-                                  Text(
-                                    data.emoji,
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                  Text(
-                                    data.dialCode,
-                                    style: TextStyle(fontSize: 14),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: Form(
-                              key: formKey,
-                              child: TextFormField(
-                                controller: textController,
-                                autofocus: true,
-                                keyboardType: TextInputType.phone,
-                                onChanged: (v) {
-                                  setState(() {
-                                    phoneNumber =
-                                        data.dialCode + textController.text;
-                                  });
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Sign up",
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 22),
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    "Access the city’s most coveted spots, luxury deals, and curated experiences.",
+                    style: TextStyle(fontSize: 14, color: Color(0xff7A7F99)),
+                  ),
+                  SizedBox(height: 16),
+                  Consumer(
+                    builder: (context, ref, child) {
+                      final selectedCountry = ref.watch(
+                        countryPickerNotifierProvider,
+                      );
+                      return selectedCountry.when(
+                        data: (data) {
+                          return Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            spacing: 8,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  showModalBottomSheet(
+                                    context: context,
+                                    isScrollControlled: true,
+                                    builder: (context) =>
+                                        CountryPickerBottomSheet(),
+                                  );
                                 },
-                                validator: (v) {
-                                  if (v!.isEmpty) {
-                                    return "please write phone number";
-                                  }
-
-                                  return null;
-                                },
-                                inputFormatters: [
-                                  FilteringTextInputFormatter.allow(
-                                    RegExp(r'[0-9+]+'),
+                                child: Container(
+                                  height: 80,
+                                  padding: EdgeInsets.all(15),
+                                  decoration: BoxDecoration(
+                                    color: Theme.of(
+                                      context,
+                                    ).colorScheme.secondaryContainer,
+                                    borderRadius: BorderRadius.circular(16),
                                   ),
-                                ],
-                                maxLength: 10,
-
-                                onFieldSubmitted: (v) async {
-                                  if (formKey.currentState!.validate()) {
-                                    await ref
-                                        .read(authServiceProvider)
-                                        .sendOTP(phoneNumber!);
-                                    if (!context.mounted) return;
-                                    context.push("/otp");
-                                  }
-                                },
-
-                                autovalidateMode:
-                                    AutovalidateMode.onUserInteraction,
-                                decoration: InputDecoration(
-                                  labelText: "Phone Number",
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    spacing: 10,
+                                    children: [
+                                      Text(
+                                        data.emoji,
+                                        style: TextStyle(fontSize: 20),
+                                      ),
+                                      Text(
+                                        data.dialCode,
+                                        style: TextStyle(fontSize: 14),
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                          ),
-                        ],
+                              Expanded(
+                                child: Form(
+                                  key: formKey,
+                                  child: TextFormField(
+                                    controller: textController,
+                                    keyboardType: TextInputType.phone,
+                                    textInputAction: TextInputAction.done,
+                                    onChanged: (v) {
+                                      setState(() {
+                                        phoneNumber =
+                                            data.dialCode + textController.text;
+                                      });
+                                    },
+                                    validator: (v) {
+                                      if (v!.isEmpty) {
+                                        return "please write phone number";
+                                      }
+                                      return null;
+                                    },
+                                    inputFormatters: [
+                                      FilteringTextInputFormatter.allow(
+                                        RegExp(r'[0-9+]+'),
+                                      ),
+                                    ],
+                                    maxLength: 10,
+                                    onFieldSubmitted: (v) async {
+                                      if (formKey.currentState!.validate()) {
+                                        await ref
+                                            .read(authServiceProvider)
+                                            .sendOTP(phoneNumber!);
+                                        if (!context.mounted) return;
+                                        context.push("/otp");
+                                      }
+                                    },
+
+                                    autovalidateMode:
+                                        AutovalidateMode.onUserInteraction,
+                                    decoration: InputDecoration(
+                                      labelText: "Phone Number",
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                        error: (e, s) {
+                          return Center(child: Text("Error"));
+                        },
+                        loading: () =>
+                            Center(child: CircularProgressIndicator()),
                       );
                     },
-                    error: (e, s) {
-                      return Center(child: Text("Error"));
-                    },
-                    loading: () => Center(child: CircularProgressIndicator()),
-                  );
-                },
+                  ),
+                  SizedBox(height: 8),
+                  Text.rich(
+                    TextSpan(
+                      text: "By continuing, you agree to to Hyde’x ",
+                      style: TextStyle(color: Color(0xff7A7F99)),
+                      children: [
+                        TextSpan(
+                          text: "Privacy ",
+                          recognizer: TapGestureRecognizer()..onTap = () {},
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                        TextSpan(text: "and "),
+                        TextSpan(
+                          text: "Terms",
+                          recognizer: TapGestureRecognizer()..onTap = () {},
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                ],
               ),
-              SizedBox(height: 8),
-              Text.rich(
-                TextSpan(
-                  text: "By continuing, you agree to to Hyde’x ",
-                  style: TextStyle(color: Color(0xff7A7F99)),
-                  children: [
-                    TextSpan(
-                      text: "Privacy ",
-                      recognizer: TapGestureRecognizer()..onTap = () {},
-                      style: TextStyle(decoration: TextDecoration.underline),
-                    ),
-                    TextSpan(text: "and "),
-                    TextSpan(
-                      text: "Terms",
-                      recognizer: TapGestureRecognizer()..onTap = () {},
-                      style: TextStyle(decoration: TextDecoration.underline),
-                    ),
-                  ],
+              Padding(
+                padding: const EdgeInsets.only(bottom: 250),
+                child: Consumer(
+                  builder: (context, ref, child) {
+                    return PrimaryButton(
+                      onTap: phoneNumber != null
+                          ? () async {
+                              if (formKey.currentState!.validate()) {
+                                await ref
+                                    .read(authServiceProvider)
+                                    .sendOTP(phoneNumber!);
+
+                                if (!context.mounted) return;
+
+                                context.push("/otp");
+                              }
+                            }
+                          : null,
+                    );
+                  },
                 ),
               ),
-              SizedBox(height: 16),
             ],
           ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: 250),
-            child: Consumer(
-              builder: (context, ref, child) {
-                return PrimaryButton(
-                  onTap: phoneNumber != null
-                      ? () async {
-                          if (formKey.currentState!.validate()) {
-                            await ref
-                                .read(authServiceProvider)
-                                .sendOTP(phoneNumber!);
-
-                            if (!context.mounted) return;
-
-                            context.push("/otp");
-                          }
-                        }
-                      : null,
-                );
-              },
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
