59c29e5956483300253f1d822eb815205a365d93
MezoPeeta <wildlifemain1@gmail.com>
2025-09-15 11:46:07 +0300

    added sentry

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ import 'package:hydex/core/notification/notification.dart';
 import 'package:hydex/firebase_options.dart';
 import 'package:hydex/src/app.dart' show MyApp;
 import 'package:device_preview/device_preview.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,12 +20,21 @@ void main() async {
     await FlutterDisplayMode.setHighRefreshRate();
   }
   await CacheHelper.init();
-  runApp(
-    DevicePreview(
-      enabled: !kReleaseMode,
-      builder: (context) {
-        return ProviderScope(child: const MyApp());
-      },
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://32c622c74f363e4bb9e092dd8263ad6b@o4510022729269248.ingest.de.sentry.io/4510022730711120';
+      // Adds request headers and IP for users,
+      // visit: https://docs.sentry.io/platforms/dart/data-management/data-collected/ for more info
+      options.sendDefaultPii = true;
+    },
+    appRunner: () => runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) {
+          return ProviderScope(child: const MyApp());
+        },
+      ),
     ),
   );
   AuthService.initialize();
